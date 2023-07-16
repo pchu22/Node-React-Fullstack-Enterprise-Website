07c0f718c1bbe5f7b80539a5554dd3298127014b
Carreiras01 <pmdc2001@gmail.com>
2023-07-16 13:33:01 +0100

    teste

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -17,6 +17,9 @@ router.post('/ativacao/:verificationToken', AuthController.verificarEmail);
 router.post('/forgot-password', AuthController.forgotPassword);
 router.post('/reset-password/:recoverToken', AuthController.mudarPassword);
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/google/redirect', passport.authenticate('google', { prompt: 'select_account', session: false }), AuthController.googleLogin);
+router.get('/google/redirect', passport.authenticate('google', { prompt: 'select_account', session: false }),
+(req,res) => {
+    res.send("redirect URI")
+});
 
 module.exports = router;
